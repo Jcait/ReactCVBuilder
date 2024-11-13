3f346fc96557e0c9a3a68ec99fcbdb2ca6b4c069
Jo <134626490+Jcait@users.noreply.github.com>
2024-11-13 12:16:55 +0000

    Add Career form input

--- src/CareerInput.jsx
+++ src/CareerInput.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 export default function CareerInput() {
+  const [careerDetails, setCareerDetails] = useState("");
   function CareerForm() {
-    <form>
-      <p>Hi</p>
-    </form>;
+    return (
+      <>
+        {" "}
+        <form>
+          <label htmlFor="jobname">Job Name</label>
+          <input type="text" name="jobname" id="jobname"></input>
+          <label htmlFor="startdate">Start Date</label>
+          <input type="date" name="startdate" id="startdate"></input>
+          <label>End Date</label>
+          <input type="date" name="enddate" id="enddate"></input>
+          <label htmlFor="jobdesc">Job Details</label>
+          <textarea name="jobdesc" id="jobdesc"></textarea>
+        </form>
+      </>
+    );
   }
   return (
     <>
