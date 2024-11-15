5e583eb52eba3723ec081add686b9c38f57c8517
Jo <134626490+Jcait@users.noreply.github.com>
2024-11-15 14:51:57 +0000

    format more

--- src/ListDetails.jsx
+++ src/ListDetails.jsx
@@ -3,7 +3,9 @@ import keySet from "./keySet";
 export default function ListDetails({ user, sectionName }) {
   const userInfo = Object.entries(user).map(([key, value]) => (
     <div key={key}>
-      {keySet(key)} : {value}
+      <p>
+        {keySet(key)} : {value}
+      </p>
     </div>
   ));
   return <section className={sectionName}>{userInfo}</section>;
